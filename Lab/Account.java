aa409416d258059eaddb14f1d41973bd13c63338
prathamkandari <prathamkandari123@gmail.com>
2022-09-16 11:54:05 +0530

    Experiment 5 Two

---
+++ Lab/Account.java
@@ -0,0 +1,9 @@
+package Balance;
+import java.lang.*;
+public class Account 
+{
+    public void Display_Balance(int balance)
+    {
+        System.out.println("Your current balance is "+balance);
+    } 
+} 
---
+++ Lab/Output.java
@@ -0,0 +1,10 @@
+package Result;
+import Balance.*;
+class Output
+{
+    public static void main(String args[])
+    {
+        Account obj = new Account();
+        obj.Display_Balance(100000);
+    }
+} 
