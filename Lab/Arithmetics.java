53c6b21186e61ab107ef0eef47f3a3a432220bed
prathamkandari <prathamkandari123@gmail.com>
2022-09-16 11:43:48 +0530

    Packages

---
+++ Lab/Arithmetics.java
@@ -0,0 +1,9 @@
+package Mathematics;
+import java.lang.*;
+public class Arithmetics
+{
+    public int add(int x,int y)
+    {
+        return x+y;
+    }
+}
---
+++ Lab/Demo.java
@@ -0,0 +1,11 @@
+package Calculator;
+import Mathematics.*;
+class Demo
+{
+        public static void main(String args[])
+    {
+            Arithmetics obj = new Arithmetics();
+            int a = obj.add(2,5);
+            System.out.println(a);
+    }
+}
