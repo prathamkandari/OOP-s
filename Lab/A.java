47d9d8d0d58f9042212dbede6aa71ac29e031673
prathamkandari <prathamkandari123@gmail.com>
2022-09-16 12:20:38 +0530

    Experiment 5

---
+++ Lab/A.java
@@ -0,0 +1,25 @@
+package p;
+import java.lang.*;
+public class A
+{
+    public void add1(int x, int y)
+    {
+        int result = x+y;
+        System.out.println(result);
+    }
+    private void add2(int x, int y)
+    {
+        int result = x+y;
+        System.out.println(result);
+    }
+    void add3(int x,int y)
+    {
+        int result = x+y;
+        System.out.println(result);    
+    }
+    protected void add4(int x,int y)
+    {
+        int result = x+y;
+        System.out.println(result);
+    }
+}
---
+++ Lab/ExperimentFive.java
@@ -0,0 +1,13 @@
+package q;
+import p.*;
+class ExperimentFive extends A
+{
+    public static void main(String args[])
+    {
+        ExperimentFive obj = new ExperimentFive();
+        obj.add1(2,4);
+        obj.add2(1,2);
+        obj.add3(6,9);
+        obj.add4(4,6);
+    }
+}
